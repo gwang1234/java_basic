989de1a433b55df5ee44fbaf6bf8c5a8c6773718
gwang1234 <qoi11@naver.com>
2025-02-17 15:09:54 +0900

    패키지

---
+++ src/pack/a/User.java
@@ -0,0 +1,8 @@
+package pack.a;
+
+public class User {
+
+    public User() {
+        System.out.println("패티지 pack a 생성");
+    }
+}
---
+++ src/pack/a/User2.java
@@ -0,0 +1,8 @@
+package pack.a;
+
+public class User2 {
+
+    public User2() {
+        System.out.println("패티지 pack a 생성");
+    }
+}
